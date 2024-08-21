47b8eb8697e85c2f3d48f13b999cb0da092e94e7
01Barthez <kenwoubarthez@gmail.com>
2024-08-21 18:00:59 +0100

    end of basic settings, defination of usually functions for authentification and mail, defination of prisma schema

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,18 @@ import rateLimit from 'express-rate-limit';
 import { setupSwagger } from './swagger';
 import morgan from 'morgan';
 import { ONE_HUNDRED, SIXTY } from './core/constants';
+import helmet from 'helmet'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
+
+// Securisations
+app.use(helmet())
+app.use(cors())
+
+//globaux
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
@@ -18,8 +28,49 @@ app.use(
 		message: 'Trop de Requete à partir de cette adresse IP '
 	})
 );
+app.use(cookieParser());
 
+// Routes du programme
+app.use(
+	"/users",
+	rateLimit({
+		max: 20,
+		windowMs: 6000,
+		handler: (req, res) => {
+			res.status(429).json({msg: "Too much request from this address"})
+		}
+	}),
+	// employee
+);
+
+app.use(
+	"/tables",
+	rateLimit({
+		max: 20,
+		windowMs: 6000,
+		handler: (req, res) => {
+			res.status(429).json({msg: "Too much request from this address"})
+		}
+	}),
+	// attendance
+);
+
+app.use(
+	rateLimit({
+		max: 20,
+		windowMs: 6000,
+		handler: (req, res) => {
+			res.status(429).json({msg: "Too much request from this address"})
+		}
+	}),
+	// abscence
+);
+
+// Journalisations
 app.use(morgan('combined'));
 
+// Documentation
 setupSwagger(app);
+
+// Export application to app file
 export default app;
